126e43787daaaba25ccb663be8baa8643a93c069
sumitsharansatsangi <sharansumitkumar@gmail.com>
2023-09-08 12:43:18 +0530

    moved to gradle 8 and uuid 4

--- android/src/main/kotlin/com/jhomlala/catcher/CatcherPlugin.kt
+++ android/src/main/kotlin/com/jhomlala/catcher/CatcherPlugin.kt
@@ -1,13 +1,10 @@
 package com.jhomlala.catcher
 
-import androidx.annotation.NonNull
-
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 
 /** CatcherPlugin */
 
@@ -17,12 +14,12 @@ class CatcherPlugin: FlutterPlugin, MethodCallHandler {  /// The MethodChannel t
   /// when the Flutter Engine is detached from the Activity
   private lateinit var channel : MethodChannel
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+  override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "catcher")
     channel.setMethodCallHandler(this)
   }
 
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+  override fun onMethodCall(call: MethodCall, result: Result) {
     if (call.method == "getPlatformVersion") {
       result.success("Android ${android.os.Build.VERSION.RELEASE}")
     } else {
@@ -30,7 +27,7 @@ class CatcherPlugin: FlutterPlugin, MethodCallHandler {  /// The MethodChannel t
     }
   }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
     channel.setMethodCallHandler(null)
   }
 }
